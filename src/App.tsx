3ac77b36dd162c08d8de5ec2bceb565f3600926b
muhammad-fiaz <s.muhammadfiaz2003@gmail.com>
2023-06-06 19:34:46 +0530

    Added React Support

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Notice from "./components/dev/status";
+
+function App() {
+  return (
+        <Notice />
+  );
+}
+
+export default App;
---
+++ src/components/dev/status.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Notice: React.FC = () => {
+    return (
+        <div className="d-flex align-items-center justify-content-center vh-100">
+            <h2 className="text-center">Coming Soon</h2>
+        </div>
+    );
+}
+
+export default Notice;
