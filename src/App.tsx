364ebb847b80cb863de1b90298143763190b7dce
muhammad-fiaz <s.muhammadfiaz2003@gmail.com>
2023-06-09 14:27:37 +0530

    Some Minor Issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Navigate, BrowserRouter} from 'react-router-dom';
 import Home from './components/home/Home';
 import About from './components/pages/About';
 import Services from './components/pages/Services';
 import Projects from './components/pages/Projects';
 import Contact from './components/pages/Contact';
 import LoadingScreen from "./components/splash/splash";
-import Navbar from "./components/home/Navbar";
-import Notice from "./components/dev/status"; // Corrected import statement
 
 const App: React.FC = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -47,17 +45,17 @@ const App: React.FC = () => {
             ) : (
                 <div>
                     {isLoading && <LoadingScreen />}
-                    <Router>
-                        <Navbar />
-                        <Notice />
+                    <BrowserRouter>
                         <Routes>
                             <Route path="/" element={<Home />} />
                             <Route path="/about" element={<About />} />
                             <Route path="/services" element={<Services />} />
                             <Route path="/projects" element={<Projects />} />
-                            <Route path="/contact" element={<Contact />} /> {/* Updated component name */}
+                            <Route path="/contact" element={<Contact />} />
+                            <Route path="*" element={<Navigate to="/404" />} />
                         </Routes>
-                    </Router>
+                    </BrowserRouter>
+
                 </div>
             )}
         </div>
