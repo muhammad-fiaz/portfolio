af59c3827b21a8517ff6fb0be7c0ad028cac2fea
muhammad-fiaz <s.muhammadfiaz2003@gmail.com>
2023-06-09 16:23:03 +0530

    Some Minor Issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {BrowserRouter as Router, Routes, Route, Navigate, BrowserRouter} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/home/Home';
 import About from './components/pages/About';
 import Services from './components/pages/Services';
@@ -9,43 +9,26 @@ import LoadingScreen from "./components/splash/splash";
 
 const App: React.FC = () => {
     const [isLoading, setIsLoading] = useState(true);
-    const [isInitialLoad, setIsInitialLoad] = useState(true);
 
     useEffect(() => {
-        const minimumDuration = 3000; // Minimum duration in milliseconds
-        const startTime = Date.now();
-
         const handleLoad = () => {
-            const elapsedTime = Date.now() - startTime;
-            const remainingTime = Math.max(minimumDuration - elapsedTime, 0);
-            setTimeout(() => {
-                setIsLoading(false);
-                setIsInitialLoad(false);
-            }, remainingTime);
-        };
-
-        const handle404Error = () => {
             setIsLoading(false);
-            setIsInitialLoad(false);
         };
 
         window.addEventListener('load', handleLoad);
-        window.addEventListener('error', handle404Error);
 
         return () => {
             window.removeEventListener('load', handleLoad);
-            window.removeEventListener('error', handle404Error);
         };
     }, []);
 
     return (
         <div className="app-container">
-            {isLoading && isInitialLoad ? (
+            {isLoading ? (
                 <LoadingScreen />
             ) : (
                 <div>
-                    {isLoading && <LoadingScreen />}
-                    <BrowserRouter>
+                    <Router>
                         <Routes>
                             <Route path="/" element={<Home />} />
                             <Route path="/about" element={<About />} />
@@ -54,8 +37,7 @@ const App: React.FC = () => {
                             <Route path="/contact" element={<Contact />} />
                             <Route path="*" element={<Navigate to="/404" />} />
                         </Routes>
-                    </BrowserRouter>
-
+                    </Router>
                 </div>
             )}
         </div>
