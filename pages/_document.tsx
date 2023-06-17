602f0870df7cfb3c83261104595d134531f576e2
muhammad-fiaz <s.muhammadfiaz2003@gmail.com>
2023-06-17 22:44:28 +0530

    Minor Bug Fix nothing else

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,21 +29,36 @@ export default function Document() {
 				<meta name="theme-color" content="#ffffff" />
 				<link rel="manifest" href="/manifest.json" />
 				<title>Muhammad Fiaz</title>
+
+				{/* Google Analytics */}
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-SDJ0K1Y70X"></script>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag() {
+                                dataLayer.push(arguments);
+                            }
+                            gtag('js', new Date());
+                            gtag('config', 'G-SDJ0K1Y70X');
+                        `
+					}}
+				/>
+
+				{/* Clarity Analytics */}
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+                            (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "hle8ty6om9");
+                        `
+					}}
+				/>
 			</Head>
-			{/* Google Analytics */}
-			<script async src="https://www.googletagmanager.com/gtag/js?id=G-SDJ0K1Y70X"></script>
-			<script
-				dangerouslySetInnerHTML={{
-					__html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag() {
-                            dataLayer.push(arguments);
-                        }
-                        gtag('js', new Date());
-                        gtag('config', 'G-SDJ0K1Y70X');
-                    `
-				}}
-			/>
+
 			<body>
 			<Main />
 			<NextScript />
