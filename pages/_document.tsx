ace4015158f2fa94279bf7643894a7ca2d2b1ccf
Muhammad Fiaz <s.muhammadfiaz2003@gmail.com>
2023-06-25 17:46:46 +0530

    Issue Fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -55,9 +55,9 @@ export default function Document() {
 				/>
 
 				{/* Google Adsense */}
-				<Script async
+				<script async
 						src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2040560600290490"
-						crossOrigin="anonymous"></Script>  {/* use your own adsense id or else remove this line */}
+						crossOrigin="anonymous"></script>  {/* use your own adsense id or else remove this line */}
 
 				{/* Clarity Analytics */}
 				<script
