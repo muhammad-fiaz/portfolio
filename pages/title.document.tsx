503d45ff70054b81555c604084639296d04e3ba6
Muhammad Fiaz <75434191+muhammad-fiaz@users.noreply.github.com>
2023-09-03 22:38:10 +0530

    Update title.document.tsx

--- pages/title.document.tsx
+++ pages/title.document.tsx
@@ -50,6 +50,28 @@ export default function TitleDocument() {
 
 
 
+  <meta property="og:title" content="Muhammad Fiaz" key="title" />
+            <meta property="og:description" content="Muhammad Fiaz - A dedicated Full Stack Developer proficient in App/Web, Cloud, DevOps, AI/ML, and Design. Join my journey." />
+            <meta property="og:image" content="https://muhammadfiaz.com/img/fiaz-org.jpeg" />
+            <meta property="og:image" content="https://muhammadfiaz.com/img/fiaz_roundedImage.png" />
+            <meta property="og:image:secure_url" content="https://muhammadfiaz.com/img/fiaz-org.jpeg" />
+            <meta property="og:image:type" content="image/jpeg" />
+            <meta property="og:image:alt" content="Muhammad Fiaz" />
+            <meta property="og:image:width" content="300" />
+            <meta property="og:image:height" content="300" />
+            <meta property="og:url" content="https://muhammadfiaz.com" />
+            <meta property="og:type" content="website" />
+            <meta property="og:profile" content="https://github.com/muhammad-fiaz" />
+            <meta property="og:site_name" content="Muhammad Fiaz" />
+
+
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:site" content="https://twitter.com/muhammadfiaz_" />
+            <meta name="twitter:title" content="Muhammad Fiaz" />
+            <meta name="twitter:description" content="Muhammad Fiaz - A dedicated Full Stack Developer proficient in App/Web, Cloud, DevOps, AI/ML, and Design. Join my journey." />
+            <meta name="twitter:image" content="https://muhammadfiaz.com/img/fiaz-org.jpeg" />
+            <meta name="twitter:creator" content="https://twitter.com/muhammadfiaz_" />
+            <meta name="twitter:domain" content="https://muhammadfiaz.com" />
 
         </Head>
     );
