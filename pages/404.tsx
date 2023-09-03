5134723fb54ed6fbe007e0e95062d5df75a5b16a
Muhammad Fiaz <s.muhammadfiaz2003@gmail.com>
2023-09-03 12:22:25 +0530

    Added Custom title to 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,10 @@
 import Custom404 from "../src/components/blocks/errors/custom404";
+import Title404 from "./title.404";
 
 export default function Notfound() {
     return (
         <div>
+            <Title404/>
             <Custom404/>
         </div>
     );
--- pages/title.document.tsx
+++ pages/title.document.tsx
@@ -35,11 +35,13 @@ export default function TitleDocument() {
             <meta name="googlebot" content="index, follow" />
             <meta name="bingbot" content="index, follow" />
 
-
             <link rel="apple-touch-icon" sizes="180x180" href="/favicon/favicon.jpg" />
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon.ico" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon.jpg" />
-            <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+             <link rel="icon" href="/favicon/favicon-org.ico" type="image/x-icon"></link>
+            <link rel="shortcut icon" href="/favicon/favicon-org.ico" type="image/x-icon"></link>
+
+             <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
             <meta name="msapplication-TileColor" content="#da532c" />
             <meta name="theme-color" content="#ffffff" />
             <link rel="manifest" href="/manifest.json" />
