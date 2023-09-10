c6c99e7f71826a43803eb16e24a2031f91eb45cd
Muhammad Fiaz <75434191+muhammad-fiaz@users.noreply.github.com>
2023-09-10 15:25:27 +0530

    Some Minor Improvement and Changes✅ (#135)
    * Added Custom500 and Service Worker👻
    
    * minor changes
    
    * Readme.md Got minor Changes and add SUPPORTME.md✅
    
    * Minor Changes and Improvements

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,6 @@
-import Custom404 from "../src/components/blocks/errors/custom404";
+const Custom404   = dynamic(import ( "../src/components/blocks/errors/custom404"));
 import Title404 from "./title.404";
+import dynamic from "next/dynamic";
 
 export default function Notfound() {
     return (
