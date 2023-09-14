896f6f8bcfc864c0a69346f60fd29c5283053eb3
Muhammad Fiaz <s.muhammadfiaz2003@gmail.com>
2023-09-14 22:54:46 +0530

    Added PurgeCSS

--- next.config.js
+++ next.config.js
@@ -7,7 +7,31 @@ const withPWA = require('next-pwa')({
 })
 
 module.exports =  withPWA({
-
+	"plugins": [
+		"postcss-flexbugs-fixes",
+		[
+			"postcss-preset-env",
+			{
+				"autoprefixer": {
+					"flexbox": "no-2009"
+				},
+				"stage": 3,
+				"features": {
+					"custom-properties": false
+				}
+			}
+		],
+		[
+			'@fullhuman/postcss-purgecss',
+			{
+				content: [
+					'./pages/**/*.{js,jsx,ts,tsx}',
+					'./src/components/**/*.{js,jsx,ts,tsx}'
+				],
+				defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+				safelist: ["html", "body"]
+			}
+		],],
 	webpack: (config) => {
 		// Add your custom Webpack configurations here
 
