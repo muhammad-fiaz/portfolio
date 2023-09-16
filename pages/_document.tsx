3118f9371a5c000a34e819377368982342e4011b
Muhammad Fiaz <75434191+muhammad-fiaz@users.noreply.github.com>
2023-09-16 13:38:01 +0530

    Merge the Latest Changes✅ (#152)
    * Added Custom500 and Service Worker👻
    
    * minor changes
    
    * Readme.md Got minor Changes and add SUPPORTME.md✅
    
    * Minor Changes and Improvements
    
    * Minor Fix
    
    * Dep upgrade✅
    
    * Removed settings and Some Minor Random Improvement and Changes✅
    
    * Added PurgeCSS
    
    * Some Small Changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -241,17 +241,19 @@ export default function Document() {
           }
         `
             }} />
+
             {/*Edit this to your according to your website*/}
             <script type="application/ld+json" dangerouslySetInnerHTML={{
                 __html: `
           {
             "@context": "https://schema.org",
             "@type": "ResearchProject",
-            "name": "Muhammad Fiaz",
-            "alternateName": "fiaz",
-            "url": "https://muhammadfiaz.com",
-            "logo": "https://muhammadfiaz.com/img/muhammadfiaz.jpeg",
- "sameAs": [
+            "name": "NeuroLink",
+            "description": "NeuroLink is a Python package that aims to provide various AI capabilities, including a chatbot, image processing, audio processing, Django support, and more. It utilizes TensorFlow and other related technologies for natural language processing and AI tasks.",
+            "alternateName": "neurolink",
+            "url": "https://github.com/muhammad-fiaz/neurolink",
+            "logo": "https://private-user-images.githubusercontent.com/75434191/242214243-82fbf702-43e2-46e6-8e01-11758fa26310.gif?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE2OTQ4NTE2ODksIm5iZiI6MTY5NDg1MTM4OSwicGF0aCI6Ii83NTQzNDE5MS8yNDIyMTQyNDMtODJmYmY3MDItNDNlMi00NmU2LThlMDEtMTE3NThmYTI2MzEwLmdpZj9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzA5MTYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMwOTE2VDA4MDMwOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWQ3NjJmNzA5MGU5NWY4NTUxZTcxYmVmNzNlZTYzOTViN2FjNzE1YTM2MjM1OTNlOGIxNzk0ODE1NDczMDMzNGEmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.ay5bRLRoojQ33ZJLFnvjyq6Dq4qZfDY7quvv4WmgRLI",
+             "sameAs": [
             "https://www.linkedin.com/in/muhammad-fiaz-/"
         ]          }
         `
