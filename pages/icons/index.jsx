f7c678d42878ed2530142c4053d1f99d2bbddcfd
Muhammad Fiaz <s.muhammadfiaz2003@gmail.com>
2023-10-01 13:49:03 +0530

    Updated Copyrights and Minor Changes✅

--- pages/icons/index.jsx
+++ pages/icons/index.jsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 [Muhammad Fiaz](https://github.com/muhammad-fiaz/portfolio)
+ * Copyright (c) 2023 [Muhammad Fiaz](https://github.com/muhammad-fiaz/)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
