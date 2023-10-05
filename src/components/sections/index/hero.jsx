954d3597d52ade11918b2eb65462d12564f69dca
Muhammad Fiaz <s.muhammadfiaz2003@gmail.com>
2023-10-05 22:13:59 +0530

    Improved Hero UI

--- src/components/sections/index/hero.jsx
+++ src/components/sections/index/hero.jsx
@@ -1,9 +1,9 @@
-
-import {useState} from 'react';
-import {TypeAnimation} from 'react-type-animation';
+import React, { useState } from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 import Section from '../../structure/section';
 import Container from '../../structure/container';
+import Image from 'next/image';
 
 import space from '../../utils/spacing';
 
@@ -12,26 +12,35 @@ import HeroBg from '../../blocks/hero.bg/section-bg-color';
 import hero from '../../../../assets/styles/scss/sections/index/hero.module.scss';
 import button from '../../../../assets/styles/scss/blocks/button.module.scss';
 
-import content from '../../../content/index/hero.json'
-
+import content from '../../../content/index/hero.json';
 
 export default function Hero() {
-
 	const [, setTypingStatus] = useState('Initializing');
+	const [isHiremeTooltipVisible, setIsHiremeTooltipVisible] = useState(false);
+	const [isYescatTooltipVisible, setIsYescatTooltipVisible] = useState(false);
 
 	return (
 		<Section classProp={`${hero.section}`}>
 			<Container spacing={'VerticalXXXL'}>
-				<TypeAnimation className={`${hero.preHeader}`}
+				<TypeAnimation
+					className={`${hero.preHeader}`}
 					sequence={[
 						content.intro.startDelay,
-						() => { setTypingStatus('typing') },
+						() => {
+							setTypingStatus('typing');
+						},
 						content.intro.start,
-						() => {	setTypingStatus('typed') },
+						() => {
+							setTypingStatus('typed');
+						},
 						content.intro.deleteDelay,
-						() => {	setTypingStatus('deleting') },
+						() => {
+							setTypingStatus('deleting');
+						},
 						content.intro.end,
-						() => {	setTypingStatus('deleted') },
+						() => {
+							setTypingStatus('deleted');
+						},
 						content.intro.restartDelay,
 					]}
 					speed={content.intro.speed}
@@ -40,30 +49,44 @@ export default function Hero() {
 					repeat={Infinity}
 				/>
 				<section>
-					<h1 className={hero.header}>
-						{content.header.name}
-						</h1>
-					<h1 className={`${hero.header} ${hero.primaryDim}`}>
-						{content.header.usp}
-					</h1>
+					<h1 className={hero.header}>{content.header.name}</h1>
+					<h1 className={`${hero.header} ${hero.primaryDim}`}>{content.header.usp}</h1>
 				</section>
 				<section>
-					<p className={`${hero.primaryBright} subtitle ${space(["verticalLrg"])}`}>
-						{ content.paragraph }
-					</p>					
+					<p className={`${hero.primaryBright} subtitle ${space(['verticalLrg'])}`}>
+						{content.paragraph}
+					</p>
 				</section>
-				<section>
-					<button	className={`button ${button.primary}`}
-							onClick={ () => window.location = 'mailto:s.muhammadfiaz2003@gmail.com' } >
+				<section style={{ position: 'relative' }}>
+					<button
+						className={`button ${button.primary}`}
+						onClick={() => (window.location = 'mailto:s.muhammadfiaz2003@gmail.com')}
+						onMouseEnter={() => setIsHiremeTooltipVisible(true)}
+						onMouseLeave={() => setIsHiremeTooltipVisible(false)}
+					>
 						{content.buttons.primary.title}
 					</button>
-					<button className={`button ${button.secondary} leaveSite`}
-							onClick={ ()=> window.open("https://www.linkedin.com/in/muhammad-fiaz-/", "_blank") } >
+					{isHiremeTooltipVisible && (
+						<div className={`${hero.tooltipContent} ${hero.hireme_tooltip}`}>
+							<Image src="/gif/hireme.gif" width={100} height={100} alt="Hireme GIF" />
+						</div>
+					)}
+					<button
+						className={`button ${button.secondary} leaveSite`}
+						onClick={() => window.open('https://www.linkedin.com/in/muhammad-fiaz/', '_blank')}
+						onMouseEnter={() => setIsYescatTooltipVisible(true)}
+						onMouseLeave={() => setIsYescatTooltipVisible(false)}
+					>
 						{content.buttons.secondary.title}
 					</button>
+					{isYescatTooltipVisible && (
+						<div className={`${hero.tooltipContent} ${hero.yescat_tooltip}`}>
+							<Image src="/gif/yes.gif" width={100} height={100} alt="Yescat GIF" />
+						</div>
+					)}
 				</section>
 			</Container>
 			<HeroBg theme="bg-color-1" />
 		</Section>
-	)
+	);
 }
