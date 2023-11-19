a6a18bdcd65057a14766271f585ad20397d85bc2
Muhammad Fiaz <contact@muhammadfiaz.com>
2023-11-19 19:21:40 +0530

    Improved Hero UI and Some Other Changes (#324)
    * dep upgraded ✅
    
    * fixed #246 and made some changes in timeline.tsx
    
    * fixed #247 issue ✅
    
    * fixed #281 issue
    
    * git merge main
    
    * Added New Feature AutoUpdateCheck #319
    
    * Improved the AutoUpdateCheck Feature
    
    * fixed the issue #302
    
    * Improved Hero Section UI

--- src/components/blocks/hero.bg/section-bg-color.tsx
+++ src/components/blocks/hero.bg/section-bg-color.tsx
@@ -1,15 +1,11 @@
-
-// Core packages
 import hero from '../../../../assets/styles/scss/sections/index/hero.module.scss';
-
 export default function sectionBgColor() {
 	return (
-		<div className={`${hero.colorfulV1}`}>
+		<div className={`${hero.heroBackground}`}>
 			<div className={`${hero.barContainer} noEvents`}>
 				<div className={`${hero.barGradient}`}></div>
 			</div>
 			<div className={`${hero.radialContainer} noEvents`}>
-				{/* <Image src="/img/dataism-24.svg" className={`${hero.svg_background}`} height={2000} width={2000} alt="Dataism 24 " /> */}
 				<div className={`${hero.radialGradient}`}></div>
 			</div>
 		</div>
