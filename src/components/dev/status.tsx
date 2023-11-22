aa4b294ce8242fe615da6cf82430a2dd8194bd76
Muhammad Fiaz <s.muhammadfiaz2003@gmail.com>
2023-11-22 22:07:26 +0530

    Added a New Content message after Verification on Status Notice

--- src/components/dev/status.tsx
+++ src/components/dev/status.tsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
 import HCaptcha from '@hcaptcha/react-hcaptcha';
@@ -12,6 +11,10 @@ const DevelopmentNotice: React.FC = () => {
     const [hasShownConfetti, setHasShownConfetti] = useState<boolean>(false);
     const [versionDetails, setVersionDetails] = useState<VersionDetails | null>(null);
     const [autoupdatecheck, setAutoupdatecheck] = useState<boolean | null>(null);
+    const [WelMsg, setWelMsg] = useState<string | null>(null);
+    const [hideContent, setHideContent] = useState<boolean>(false);
+    const [showNewPlayer, setShowNewPlayer] = useState<boolean>(false);
+    const [timerRemaining, setTimerRemaining] = useState<number | null>(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -36,8 +39,23 @@ const DevelopmentNotice: React.FC = () => {
 
     const handleHidePopup = () => {
         localStorage.setItem('developmentNotice', 'true');
-        setShowPopup(false);
-        setHasShownConfetti(true);
+        setIsVerified(true);
+        setWelMsg("Make sure to Follow me on Github for more! ❤️");
+        setHideContent(true); // Hide existing content
+        setShowNewPlayer(true); // Show new Player
+        const timeoutDuration = 5000;
+        setTimerRemaining(timeoutDuration / 1000);
+        const intervalId = setInterval(() => {
+            setTimerRemaining((prev) => (prev !== null ? prev - 1 : null));
+        }, 1000);
+
+        setTimeout(() => {
+            clearInterval(intervalId);
+            setShowPopup(false);
+            setHasShownConfetti(true);
+            setWelMsg(null);
+            setTimerRemaining(null);
+        }, timeoutDuration);
     };
 
     const handleVerify = (token: string) => {
@@ -81,50 +99,79 @@ const DevelopmentNotice: React.FC = () => {
                         textAlign: 'center',
                     }}
                 >
-                    <Player
-                        autoplay
-                        loop
-                        src="/lottie/codingdev.json"
-                        style={{ marginBottom: '20px', width: '300px', height: '300px' }}
-                    />
-                    <p style={{ fontSize: '18px', marginBottom: '20px' }}>
-                        Thank you for visiting my portfolio!👻
-                    </p>
-                    <p style={{ fontSize: '14px', marginBottom: '30px' }}>
-                        If you encounter any issues or bugs, please report them{' '}
-                        <a href="https://github.com/muhammad-fiaz/portfolio/issues/new">@muhammad-fiaz</a>
-                    </p>
-                    {!isVerified ? (
+                    {showNewPlayer ? (
+                        <Player
+                            autoplay
+                            loop
+                            src="/lottie/github.json"
+                            style={{ marginBottom: '20px', width: '300px', height: '300px' }}
+                        />
+                    ) : (
+                        <Player
+                            autoplay
+                            loop
+                            src="/lottie/codingdev.json"
+                            style={{ marginBottom: '20px', width: '300px', height: '300px' }}
+                        />
+                    )}
+                    {hideContent ? null : (
                         <>
-                            <div style={{ marginBottom: '10px', marginLeft: '20px' }}>
-                                <HCaptcha
-                                    sitekey="d27bf471-6339-4603-b63f-5ab5fdd96ace"
-                                    onVerify={handleVerify}
-                                />
-                            </div>
-                            <p style={{ fontSize: '12px', color: 'red', marginBottom: '10px' }}>
-                                Please complete the verification.
+                            <p style={{ fontSize: '18px', marginBottom: '20px' }}>
+                                Thank you for visiting my portfolio!👻
                             </p>
+                            <p style={{ fontSize: '14px', marginBottom: '30px' }}>
+                                If you encounter any issues or bugs, please report them{' '}
+                                <a href="https://github.com/muhammad-fiaz/portfolio/issues/new">@muhammad-fiaz</a>
+                            </p>
+                            {!isVerified ? (
+                                <>
+                                    <div style={{ marginBottom: '10px', marginLeft: '20px' }}>
+                                        <HCaptcha
+                                            sitekey="d27bf471-6339-4603-b63f-5ab5fdd96ace"
+                                            onVerify={handleVerify}
+                                        />
+                                    </div>
+                                    <p style={{ fontSize: '12px', color: 'red', marginBottom: '10px' }}>
+                                        Please complete the verification.
+                                    </p>
+                                </>
+                            ) : null}
                         </>
-                    ) : null}
-                    <button
-                        onClick={handleHidePopup}
-                        disabled={!isVerified}
-                        style={{
-                            background: 'linear-gradient(to right, #7b68ee, #b22cff)',
-                            color: '#ffffff',
-                            border: 'none',
-                            padding: '10px 20px',
-                            borderRadius: '5px',
-                            cursor: 'pointer',
-                            marginTop: '10px',
-                        }}
-                    >
-                        I understand
-                    </button>
+                    )}
+                    {WelMsg && (
+                        <div>
+                            <h2 style={{ fontSize: '32px', marginBottom: '20px'}}>
+                                <span dangerouslySetInnerHTML={{ __html: WelMsg.replace('Github', '<a href="https://github.com/muhammad-fiaz" target="_blank" rel="noopener noreferrer" style="color: #3498db;">Github</a>') }} />
+                            </h2>
+                            {timerRemaining !== null && (
+                                <p style={{ fontSize: '16px', marginBottom: '10px', color: 'linear-gradient(to right, #3498db, #2ecc71)' }}>
+                                    Closes in {timerRemaining} seconds.
+                                </p>
+                            )}
+                        </div>
+                    )}
 
+
+
+                    {!hideContent && (
+                        <button
+                            onClick={handleHidePopup}
+                            disabled={!isVerified}
+                            style={{
+                                background: 'linear-gradient(to right, #7b68ee, #b22cff)',
+                                color: '#ffffff',
+                                border: 'none',
+                                padding: '10px 20px',
+                                borderRadius: '5px',
+                                cursor: 'pointer',
+                                marginTop: '10px',
+                            }}
+                        >
+                            I understand
+                        </button>
+                    )}
                 </div>
-                {autoupdatecheck && ( // Render version details only if autoupdatecheck is true
+                {autoupdatecheck && (
                     <div
                         style={{
                             marginTop: '10px',
