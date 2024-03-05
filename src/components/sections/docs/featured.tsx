2cde585df4fe2c7336c1ec8e6fa70c47a28e3d0b
Muhammad Fiaz <contact@muhammadfiaz.com>
2024-03-05 23:21:52 +0530

    upgraded the deps to latest and some improvements and updated contents (#480)

--- src/components/sections/docs/featured.tsx
+++ src/components/sections/docs/featured.tsx
@@ -31,7 +31,7 @@ export default function Recent({ }) {
                                 <>
                                     <article key={index} className={css.project}>
 								<span className={css.featuredImage}>
-                                    <Image src={thumbnail} alt="Documentation thumbnail" height={400} width={600} loading="eager" />
+                                    <Image src={thumbnail} alt="Documentation thumbnail" height={300} width={300}  loading="eager" />
 								</span>
                                         <span className={css.header}>
 									<a href={link} rel="noreferrer" target="_blank">{title} <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a>
