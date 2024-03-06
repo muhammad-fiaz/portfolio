4203b3208f2124b66472ee00ec2fe9f944dd6e5d
Muhammad Fiaz <contact@muhammadfiaz.com>
2024-03-06 20:48:37 +0530

    Update _document.tsx
    fix tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -135,8 +135,13 @@ export default function Document() {
 "/>
                 {/*use your own ads sense code here*/}
 		    <meta name="google-adsense-account" content="ca-pub-2040560600290490"/>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2040560600290490"
-     crossorigin="anonymous"></script>
+		     <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2040560600290490`}
+          strategy="lazyOnload"
+          crossOrigin="anonymous"
+        />
+         
                 <script
                     type="application/ld+json"
                     dangerouslySetInnerHTML={{
