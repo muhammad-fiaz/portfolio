c9570968fde24bd044cff81ffc88a0cbbfcc3617
Muhammad Fiaz <contact@muhammadfiaz.com>
2024-12-30 20:54:46 +0530

    removed and fix issue

--- src/components/ui/ShowSkills.tsx
+++ src/components/ui/ShowSkills.tsx
@@ -8,7 +8,6 @@ const MySkills = () => {
     <div
       className={` w-full h-full flex justify-center items-center`}
     >
-      import Image from 'next/image';
 
       <Image
         src="https://skillicons.dev/icons?i=androidstudio,angular,atom,aws,azure,bash,blender,bootstrap,c,cs,cpp,cloudflare,codepen,css,dart,django,docker,dotnet,eclipse,express,figma,firebase,flask,flutter,gcp,git,github,githubactions,gitlab,go,gradle,gulp,heroku,html,idea,java,js,jquery,kotlin,linkedin,linux,md,mongodb,mysql,netlify,nextjs,nginx,nodejs,ps,php,rust,svelte,tauri,electron,postgres,powershell,py,pytorch,qt,react,redux,regex,sass,sqlite,stackoverflow,svg,tailwind,tensorflow,twitter,ts,unity,unreal,vercel,visualstudio,vite,vscode,vue,webflow,webpack,wordpress&perline=15"
