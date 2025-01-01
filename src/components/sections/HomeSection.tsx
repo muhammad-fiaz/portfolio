2883f81218829e72dc4331d1b4b2c308399b5da4
Muhammad Fiaz <contact@muhammadfiaz.com>
2025-01-01 21:56:50 +0530

    added animation effect for badge also

--- src/components/sections/HomeSection.tsx
+++ src/components/sections/HomeSection.tsx
@@ -15,13 +15,15 @@ const HomeSection = () => {
       {/* Hero Section */}
       <div className="w-full h-[calc(100vh-11rem)] flex items-center justify-center">
         {/* Badge */}
+        <AnimationContainer >
+
         <div className="absolute top-1/4 md:top-60 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-green-100 text-green-800 text-xs sm:text-sm font-medium px-4 py-2 rounded-full shadow-md">
           <span className="flex items-center gap-2">
             <span className="w-2 h-2 bg-green-500 rounded-full animate-blink-circle"></span>
             Available for new opportunities 😁
           </span>
         </div>
-
+        </AnimationContainer>
         <Hero />
       </div>
 
